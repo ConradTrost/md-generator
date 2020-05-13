85106874fbeb21a18d16947554bbe3191684225d
Conrad Trost <ctrost@tamu.edu>
2020-05-13 12:34:19 -0500

    All added except licensing badges

--- Develop/index.js
+++ Develop/index.js
@@ -3,16 +3,24 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 // array of questions for user
+// Questions: 
+// Section {TITLE}  ----->  Self explanatory
+// Sections {Description, Installation, Usage, Contributing, and Tests}  ----->  Description, installation instructions, usage info, contribution guidelines, and test instructions
+// Section {LICENSE}    ----->  Option to choose license and then the badge is added from https://shields.io
+// Section {QUESTIONS}  ----->  Github username (link to profile), Email address (w/ instructions on how to reach person)
+// Table of contents with links to the sections
+
+
 const questions = [
     {
         type: 'input',
-        name: 'name',
+        name: 'author',
         message: 'Enter your name:',
         validate: nameInput => {
             if (nameInput) {
                 return true;
             } else {
-                console.log('Please enter name! (required)');
+                console.log('Please enter your name! (required)');
                 return false;
             }
         }
@@ -29,13 +37,109 @@ const questions = [
                 return false;
             }
         }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter the project title:',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter the project title! (required)');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description of the project:',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter the description! (required)');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'Enter installation instructions:',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter installation instructions! (required)');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information:',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter usage information! (required)');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Enter contribution guidelines:',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter contribution guidelines! (required)');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Enter testing instructions:',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter testing instructions! (required)');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'confirmLicense',
+        message: 'Would you like to add a license?',
+        default: false
+    },
+    {   // -----> Add checkbox for if license is needed. Send to shields.io
+        type: 'checkbox',
+        name: 'license',
+        message: 'Enter license(s) (Check all that apply):',
+        choices: ['Apache', 'Apache 2', 'BSD', 'MIT', 'GPL', 'GPL (>= 2)'],
+        when: ({ confirmLicense }) => confirmLicense
     }
-];
+]; 
 
 // function to write README file
 function writeToFile(fileName, data) {
     return new Promise((resolve, reject) => {
-        fs.writeFile('./dist/README.md', fileContent, err => {
+        fs.writeFile(fileName, data, err => {
             if (err) {
                 reject(err);
                 return;
@@ -50,8 +154,18 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
-}
+    return inquirer
+        .prompt(questions)
+        .then(answers => {
+            console.log(answers)
+            return generateMarkdown(answers)
+        })
+        .then(templateData => {
+            writeToFile('./Develop/dist/README.md', templateData)
+        })
+};
 
 // function call to initialize program
 init();
+
+// Add badges w/ shields.io then im done
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,9 +1,54 @@
-const fs = require('fs');
+// Make generator using node "inquirer"
+// Guide to professional README located in root (readme-guide.md) or https://github.com/coding-boot-camp/potential-enigma/blob/master/readme-guide.md
+
+// array of questions for user
+// Questions: 
+// Section {TITLE}  ----->  Self explanatory
+// Sections {Description, Installation, Usage, Contributing, and Tests}  ----->  Description, installation instructions, usage info, contribution guidelines, and test instructions
+// Section {LICENSE}    ----->  Option to choose license and then the badge is added from https://shield.io
+// Section {QUESTIONS}  ----->  Github username (link to profile), Email address (w/ instructions on how to reach person)
+// Table of contents with links to the sections
+
 
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+  ### Developed by ${data.author}
 
+  ### Table of Contents:
+
+  **[Description](#description)**<br>
+  **[Installation Instructions](#installation-instructions)**<br>
+  **[Usage Instructions](#usage-instructions)**<br>
+  **[Contributions](#contributions)**<br>
+  **[Test Instructions](#test-instructions)**<br>
+  **[License](#license)**<br>
+  **[Questions](#questions)**<br>
+  **[Next Steps, Credits, Feedback, License](#next-steps)**<br>
+
+  ## Description
+  #### ${data.description}
+
+  ## Installation Instructions
+  #### ${data.install}
+
+  ## Usage Instructions 
+  #### ${data.usage}
+
+  ## Contributions
+  #### ${data.contribution}
+
+  ## Test Instructions
+  #### ${data.test}
+
+  ## License
+  #### ${data.license}
+
+  ## Questions
+
+  ### Find me on [Github](https://github.io/${data.github})
+  ### Email me with questions at ${data.email}
 `;
 }
 
